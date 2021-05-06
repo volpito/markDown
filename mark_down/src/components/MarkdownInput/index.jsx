98bea41380386e44dc3a1ef3016f93e29b09de38
volpito <mpj.rigot@gmail.com>
2021-05-06 13:58:18 +0200

    Markdown save that stays on screen

--- mark_down/src/components/MarkdownInput/index.jsx
+++ mark_down/src/components/MarkdownInput/index.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import NoteDisplay from '../NoteDisplay';
-import { useState } from 'react';
 
-function handleSave ()  {
-  localStorage.setItem('blocNote');
-}
+
 
 function MarkdownInput() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = React.useState('');
+  const [note, setNote] = React.useState('');
 
   const onChange = (event) => {
     setValue(event.target.value);
   };
 
-  const handleClick = () => {
-    console.log(value);
-    handleSave();
+  function handleSave ()  {
+    localStorage.setItem('blocNote', value);
+    setNote(localStorage.getItem('blocNote'));
+    console.log(note);
   }
+
+var thisNote = JSON.stringify(localStorage.getItem('blocNote'));
 
   return (
       <div>
-      <button onClick={handleClick}>Save</button>
+      <>Votre data sauvegardée : {thisNote}</>
+      <br /><br />
+      <button onClick={handleSave}>Save</button>
       <br /><br />
       <>Votre texte : <NoteDisplay markdownValue={value} /></>
       <textarea id="textArea" rows="30" cols="100" value={value} onChange={onChange}> 
